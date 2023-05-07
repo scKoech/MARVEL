cf431a8e01d8314e51d25738327346b961f97f4d
scKoech <cheropkoech04@gmail.com>
2023-05-07 19:58:51 +0300

    initial commit

---
+++ app/src/main/java/com/example/marvel/Login.kt
@@ -0,0 +1,50 @@
+package com.example.marvel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+    lateinit var TxtUserdetails:EditText
+    lateinit var EdtPass:EditText
+    lateinit var RedirectReg:TextView
+    lateinit var BtnLogin:Button
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        TxtUserdetails=findViewById(R.id.Txt_userdetails)
+        BtnLogin=findViewById(R.id.BtnLogin)
+        EdtPass=findViewById(R.id.Edt_PassLogin)
+        RedirectReg=findViewById(R.id.Txt_Register)
+        auth= FirebaseAuth.getInstance()
+
+        BtnLogin.setOnClickListener {
+            login()
+        }
+        RedirectReg.setOnClickListener {
+            val intent=Intent(this,MainPage::class.java)
+            startActivity(intent)
+
+            finish()
+        }
+    }
+    private fun login(){
+        val email=TxtUserdetails.text.toString()
+        val username=TxtUserdetails.text.toString()
+        val pass=EdtPass.text.toString()
+
+        auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(this){
+            if (it.isSuccessful){
+                Toast.makeText(this,"Logged in successfully",Toast.LENGTH_SHORT).show()
+            }else
+                Toast.makeText(this,"Log in failed",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.marvel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    lateinit var TxtFirstName: EditText
+    lateinit var TxtSecondName: EditText
+    lateinit var EdtPassword: EditText
+    lateinit var TxtUsername: EditText
+    lateinit var PhoneNum: EditText
+    lateinit var Txtemail: EditText
+    lateinit var Txtlogin: TextView
+    lateinit var EdtConfirmPass: EditText
+    lateinit var BtnSignUp: Button
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        TxtFirstName = findViewById(R.id.Edt_FisrtName)
+        TxtSecondName = findViewById(R.id.Edt_SecondName)
+        EdtPassword = findViewById(R.id.Edt_Pass)
+        TxtUsername = findViewById(R.id.Txt_username)
+        PhoneNum = findViewById(R.id.Edt_PhoneNumber)
+        Txtemail = findViewById(R.id.Edt_Email)
+        Txtlogin = findViewById(R.id.Txt_Login)
+        BtnSignUp = findViewById(R.id.BtnSignup)
+        EdtConfirmPass = findViewById(R.id.Edt_Confirmpass)
+        auth = Firebase.auth
+
+        BtnSignUp.setOnClickListener {
+            SignUpUser()
+        }
+        Txtlogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun SignUpUser() {
+        val firstname = TxtFirstName.text.toString()
+        val secondname = TxtSecondName.text.toString()
+        val email = Txtemail.text.toString()
+        val phonenum = PhoneNum.text.toString()
+        val username = TxtUsername.text.toString()
+        val password = EdtPassword.text.toString()
+        val confpass = EdtPassword.text.toString()
+        if (firstname.isBlank() || secondname.isBlank() || email.isBlank() || phonenum.isBlank() || username.isBlank() || password.isBlank() || confpass.isBlank()) {
+            Toast.makeText(this, "Fill missing fields", Toast.LENGTH_LONG).show()
+            return
+        } else if (password != confpass) {
+            Toast.makeText(this, "Password do not match", Toast.LENGTH_LONG).show()
+            return
+        }
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Signed up successfully", Toast.LENGTH_LONG).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Failed to create", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
