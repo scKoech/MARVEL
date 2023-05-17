28e884d050adde13fc57747adb1960965b081fd5
scKoech <cheropkoech04@gmail.com>
2023-05-17 11:47:38 +0300

    updated commit

--- app/src/main/java/com/example/marvel/Info.kt
+++ app/src/main/java/com/example/marvel/Info.kt
@@ -2,10 +2,19 @@ package com.example.marvel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class Info : AppCompatActivity() {
+    lateinit var txt_name:EditText
+    lateinit var txt_desc:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+        txt_desc=findViewById(R.id.Character_Desc)
+        txt_name=findViewById(R.id.Character_Name)
+
+
+
     }
 }
--- app/src/main/java/com/example/marvel/MainPage.kt
+++ app/src/main/java/com/example/marvel/MainPage.kt
@@ -1,11 +1,14 @@
 package com.example.marvel
 
+import android.app.DownloadManager
+import retrofit2.Call
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import retrofit2.http.GET
 import android.widget.Toast
 
 class MainPage : AppCompatActivity() {
@@ -20,13 +23,17 @@ class MainPage : AppCompatActivity() {
         Edt_EnterName=findViewById(R.id.Edt_Superhero)
         Btn_Enter=findViewById(R.id.Btn_Enter)
 
+        val characters = MarvelApiClient.getCharacters()
+        characters.forEach { character ->
+            println("${character.name}: ${character.description}")
+        }
+
 
         Btn_Enter.setOnClickListener {
-            val characters = MarvelApiClient.getCharacters()
-            characters.forEach { character ->
-                println("${character.name}: ${character.description}")
-            }
+           DownloadManager.Request: GET http://gateway.marvel.com/v1/public/comics?apikey=yourAPIKEY& // callback=callback_param
 
+            val intent=Intent(this,Info::class.java)
+            startActivity(intent)
         }
     }
 }
